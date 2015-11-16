ba3e440085bd8f4fb91d7f5fdc5d746ac74baa53
Eric Hodges <ehodges2004@gmail.com>
2015-11-16 09:09:19 -0800

    Finished Mole

--- javascript/Mole.js
+++ javascript/Mole.js
@@ -11,24 +11,33 @@
  *                 should be appended to occupiedHole when a mole emerges
  *
  */
-function Mole(minUpTime, maxUpTime){
+function Mole(minUpTime, maxUpTime) {
+    console.log('mollllleeeee!');
 
     // Give this.timeSpentUp a number value between minUpTime and maxUpTime.
     // HINT: use Mole.prototype.getRandomBetween
     /* YOUR CODE HERE */
+    this.timeSpentUp = this.getRandomBetween(minUpTime, maxUpTime);
+
 
     // this.removed needs a value
     /* YOUR CODE HERE */
 
+    this.removed = false;
+
     // this.occupiedHole needs a value. it should be a DOM element
     // HINT: use Mole.prototype.selectHole
     /* YOUR CODE HERE */
+
+    this.occupiedHole = this.selectHole();
 
     // Create an HTML element to represent the Mole
     // and save it into this.moleElement
     // Don't forget to give our mole a proper css class!
     // Don't forget to call whackThisMole if the mole is clicked!
     /* YOUR CODE HERE */
+    this.moleElement = $('<div class = "mole"> </div>');
+    this.moleElement.click(this.whackThisMole.bind(this));
 
     // Moles always emerge when they are created.
     this.emerge();
@@ -43,7 +52,19 @@ function Mole(minUpTime, maxUpTime){
  *
  */
 Mole.prototype.emerge = function() {
- /* YOUR CODE HERE */
+    /* YOUR CODE HERE */
+
+    if ($(this.occupiedHole).attr('data-hole-occupied') === 'true') {
+        return;
+    }
+
+    $(this.occupiedHole).append(this.moleElement);
+
+    $(this.occupiedHole).attr('data-hole-occupied', 'true');
+
+    setTimeout(this.removeMole.bind(this), this.timeSpentUp);
+
+    // this.moleElement.click(this.whackThisMole.bind(this));
 }
 
 /**
@@ -59,7 +80,21 @@ Mole.prototype.emerge = function() {
  * Then after one second it should remove the mole from the DOM.
  */
 Mole.prototype.whackThisMole = function() {
-    /* YOUR CODE HERE */
+    console.log(this);
+    //$(this.moleElement).className += ('.in-love');
+    $(this.moleElement).addClass('in-love');
+    //debugger;
+
+    // Love at last,
+
+    // and the player scored!
+    // (scoreboard is a global reference to a dom element)
+    var newScore = parseInt(scoreBoard.attr('data-score')) + 1;
+    scoreBoard.attr('data-score', newScore);
+    scoreBoard.html(newScore);
+
+    setTimeout(this.removeMole.bind(this), 1000);
+
 }
 
 /**
@@ -68,31 +103,61 @@ Mole.prototype.whackThisMole = function() {
  * false so that other moles can occupy the hole. 
  */
 Mole.prototype.removeMole = function() {
+        console.log(this);
+        if ($(this.occupiedHole).attr('data-hole-occupied') === 'true') {
+            $(this.occupiedHole).empty();
+            $(this.occupiedHole).attr('data-hole-occupied', 'false');
 
-}
-/**
- * Select an element from the DOM. The element must be one of the 
- * mole holes and it must have an attriute data-hole-occupied with
- * a value of false. 
- * 
- * If all those conditions are met, return an HTML element. 
- * If those conditions cannot be met (i.e. every hole is already occupied)
- * then return undefined.
- */
+
+        }
+
+
+    }
+    /**
+     * Select an element from the DOM. The element must be one of the 
+     * mole holes and it must have an attriute data-hole-occupied with
+     * a value of false. 
+     * 
+     * If all those conditions are met, return an HTML element. 
+     * If those conditions cannot be met (i.e. every hole is already occupied)
+     * then return undefined.
+     */
 Mole.prototype.selectHole = function() {
-    /* YOUR CODE HERE */
+    // get all of the mole holes
+    //select random hole via getRandomIntBetween
+    //if select #mole-field === false
+    //return HTML element
+    // else return undefined
+    var result = null;
+    var random = this.getRandomBetween(0, 8);
+    var occupied = $('.mole-hole')[random];
+    // console.log(occupied instanceof jQuery);
+
+    var occ2JQ = $(occupied);
+    // console.log(occ2JQ instanceof jQuery);
+
+    if (occ2JQ.attr("data-hole-occupied") === "false") {
+        // console.log('inside if', occ2JQ);
+
+        result = occ2JQ;
+    } else {
+        return undefined;
+    }
+
+    return result;
+
 };
 
 /**
  * This must return a random number in between min and max.
  */
 Mole.prototype.getRandomBetween = function(min, max) {
-    /* YOUR CODE HERE */
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 /**
  * This must return an integer in between min and max
  */
 Mole.prototype.getRandomIntBetween = function(min, max) {
-    /* YOUR CODE HERE */
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 };
--- javascript/main.js
+++ javascript/main.js
@@ -14,28 +14,34 @@ var gameCurrentlyActive;
 // Build the Mole Field of Dreams
 // Start the game
 //
-window.onload = function(){
-    
+window.onload = function() {
+
+    // createSingleMoleHole();
     // Read and save the query parameters from your form 
     // HINT: Fill out and use the function readQueryParams 
     /* YOUR CODE HERE */
-
+    readQueryParams();
     // Create the mole field 
     // HINT: Fill out and use the function createMoleField 
     /* YOUR CODE HERE */
+    createMoleField();
+    // var moleHole = new Mole().selectHole();
+
 
     // Get the score-board dom element, and save it into 
     // our variable scoreBoard
     /* YOUR CODE HERE */
+    scoreBoard = $('#score-board');
 
     // Get the game-starter button dom element and save it
     // into our variable gameStartButton
     /* YOUR CODE HERE */
-
+    gameStartButton = $('#game-starter');
     // Add an event listener to the gameStartButton. 
     // It should call the startGame function when the 
     // 'click' event is triggered
     /* YOUR CODE HERE */
+    gameStartButton.click(startGame);
 };
 
 // Examine document.location.search and extract the info 
@@ -49,19 +55,49 @@ window.onload = function(){
 //   ROUND_COOLDOWN (in seconds)
 function readQueryParams() {
     /* YOUR CODE HERE */
+    var search = document.location.search;
+    search = search.replace("?", "");
+    var searchParts = search.split("&");
+
+    for (var i = 0; i < searchParts.length; i++) {
+        var parts = searchParts[i].split("=");
+        // console.log(parts[0])
+        GLOBALS[parts[0]] = parts[1];
+    }
+    console.log(GLOBALS);
 }
 
 // Create and insert a 3x3 HTML table.
 // Use createSingleMoleHole() to create the <td> elements
 function createMoleField() {
     /* YOUR CODE HERE */
+    // loop 3 times
+    // create tr
+    // append 3 mole holes
+    // append the rows to the html table
+    //get html table
+    //insert tr (3)
+    //append td info
+    //append tr to table
+
+    for (var i = 0; i < 3; i++) {
+        var tr = $('<tr>');
+        for (var j = 0; j < 3; j++) {
+            tr.append(createSingleMoleHole());
+        };
+        $('#mole-field').append(tr);
+        // tr.append(createSingleMoleHole());
+        // tr.append(createSingleMoleHole());
+        // tr.append(createSingleMoleHole());
+    };
 }
 
 // Create a complex HTML snippet and return it. 
 // The element returned from this function should look exactly like this
 // <td><div data-hole-occupied="false" class="mole-hole"></div></td>
 function createSingleMoleHole() {
-    /* YOUR CODE HERE */
+    return $('<td><div data-hole-occupied="false" class="mole-hole"></div></td>');
+
 }
 
 ///
@@ -74,25 +110,24 @@ function createSingleMoleHole() {
 function startGame() {
     // Make sure a game isn't already in progress before starting a game
     // set the game to be in progress if its not.
-    if(gameCurrentlyActive){
+    if (gameCurrentlyActive) {
         return;
-    }
-    else {
-        gameCurrentlyActive = true;    
+    } else {
+        gameCurrentlyActive = true;
     }
 
     // Set the scoreboard back to zero
-    scoreBoard.setAttribute('data-score', 0);
-    scoreBoard.innerHTML = 0;
+    scoreBoard.attr('data-score', 0);
+    scoreBoard.html(0);
 
     // Hide the 
-    gameStartButton.style.visibility = "hidden";
+    gameStartButton.hide();
     initiateRound(0);
 }
 
 /**
-* Use a closure and the event loop to act every ROUND_COOLDOWN seconds
-*/
+ * Use a closure and the event loop to act every ROUND_COOLDOWN seconds
+ */
 function initiateRound(roundNumber) {
 
     // Closing over roundNumber in this context is confusing - but important.
@@ -102,16 +137,15 @@ function initiateRound(roundNumber) {
     // only to return after ROUND_COOLDOWN seconds. 
     var closureFunction = function() {
 
-        if(roundNumber < GLOBALS.NUMBER_OF_ROUNDS) {
+        if (roundNumber < GLOBALS.NUMBER_OF_ROUNDS) {
             // Create the moles 
-            for(var i = 0; i < GLOBALS.MOLES_PER_ROUND; i++) {
-                new Mole(GLOBALS.MOLE_UP_MIN*1000, GLOBALS.MOLE_UP_MAX*1000);
+            for (var i = 0; i < GLOBALS.MOLES_PER_ROUND; i++) {
+                new Mole(GLOBALS.MOLE_UP_MIN * 1000, GLOBALS.MOLE_UP_MAX * 1000);
             }
 
             // Next round, using our precious closed-over parameter 
             initiateRound(roundNumber + 1);
-        }
-        else {
+        } else {
             endGame();
         }
     };
@@ -129,7 +163,7 @@ function endGame() {
 
     // Wait a little while after final round, then put the 
     // gameStartButton back on the screen.
-    setTimeout(function(){
-        gameStartButton.style.visibility = "visible";
+    setTimeout(function() {
+        gameStartButton.show();
     }, maxTimeUp * 1000);
 }
